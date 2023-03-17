43d9e006f060902c1dde30e3089785756e42a09d
Patricia Garcia <martinez171214@gmail.com>
2023-03-17 16:42:16 +0100

    feat: added router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,14 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Login, Register } from './pages'
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
